703b84c4af68e63b338d39450119c84d1cc5ef40
Fábio Souto <fabiomsouto@gmail.com>
2012-01-23 14:16:09 +0000

    Corrected return type of update_users() function back to null.

--- user/externallib.php
+++ user/externallib.php
@@ -331,13 +331,7 @@ class moodle_user_external extends external_api {
      * @return external_description
      */
     public static function update_users_returns() {
-        return new external_multiple_structure(
-            new external_single_structure(
-                array(
-                    'id'       => new external_value(PARAM_INT, 'user id')
-                )
-            )
-        );
+        return null;
     }
 
     /**
